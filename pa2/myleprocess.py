ab22046b3a8e90189f3756d4f6aa634a83a69f73
Daniel Nguyen <nguyendaniel1312@gmail.com>
2026-09-15 21:28:27 -0700

    fix variable names and convert port to integer

--- pa2/myleprocess.py
+++ pa2/myleprocess.py
@@ -4,9 +4,9 @@ import threading
 import time
 import uuid
 
-uuid = uuid.uuid4()
+id = uuid.uuid4()
 
-print("uuid", uuid)
+print("uuid", id)
 
 file = open("pa2/config.txt")
 lines = file.read()
@@ -17,5 +17,19 @@ lines = lines.split('\n')
 server_ip, server_port = lines[0].split(',')
 client_ip, client_port = lines[1].split(',')
 
-print(server_ip, server_port)
-print(client_ip, client_port)
+server_port = int(server_port)
+lient_port = int(client_port)
+
+print("server", server_ip, server_port)
+print("client", client_ip, client_port)
+
+class Message:
+    def __init__(self, uuid, flag=0):
+        self.uuid = uuid
+        self.flag = flag
+    
+    def json(self):
+        return json.dumps({
+            "uuid": str(self.uuid),
+            "flag": self.flag
+        })
